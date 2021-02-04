d0ae204c81b1e473c1abb8a152b05a5105267aa1
dmi0003 <dmi0003@mix.wvu.edu>
2021-02-04 08:22:40 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,49 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>dfjkjfkldjfkljdklfldkflkdflhdlfk</p>
+    </div>
+    <div class ="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p class="c1">Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p style="color: red">Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
+                </div>
+            </div>
+        </div><div class="column">
+            <div class="card">
+                <img src="images/4.png">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
